8d97c325d9b13745b09f99e362da64cc6243f3ad
rtae0 <lk7250@yahoo.com>
2023-11-22 17:43:59 +0900

    react commit

---
+++ React/src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import logo from './logo.png';
+import logotxt from './logo.svg';
+import Attendance from './Attendance';
+import camera from './camera/camera.jpeg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      classStartTime: '', // 수업 시작 시간을 저장할 상태
+      attendanceTimes: [], // 출석 시간 목록을 저장할 상태
+    };
+  }
+
+  handleStartClass = () => {
+    // 현재 시간을 가져와서 출석 시간 목록에 추가
+    const currentTime = new Date();
+
+  };
+  render() {
+    const { attendanceTimes } = this.state;
+
+    // 생성된 출석 시간 목록을 옵션으로 변환
+    const attendanceOptions = attendanceTimes.map((time, index) => (
+      <option key={index} value={index}>
+        {time}
+      </option>
+    ));
+
+    return (
+      <div className="App">
+        <div className="header">
+            <img src={logotxt} className="main-logo" alt="logotxt" />
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <header className="App-header">
+          <img src={camera} className="image" alt="camera" />
+          <div className="setting-bar">
+            <div className="setting-container">
+              <div className="inline-box">
+                <p>수업시작 시간 : </p>
+                <input
+                  name="time-setting"
+                  type="time"
+                  className="info"
+                />
+              </div>
+            </div>
+            <div>
+              <button onClick={this.handleStartClass}>출석 시작</button>
+            </div>
+          </div>
+          <div>
+            <div className="setting-bar">
+              <h2>출석현황</h2>
+              <button>출석 저장</button>
+            </div>
+            <Attendance />
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ React/src/Attendance.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import profile from './profile_1.png';
+import './App.css';
+
+class Attendance extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            studentsData: [], // 학생 데이터를 저장할 배열
+            authenticationStatus: 2, // 초기 인식 상태 (0: 인증하지 않음, 1: 지문인증, 2: 얼굴인증, 3: 지문/얼굴 인증)
+        };
+    }
+
+    componentDidMount() {
+        // 서버에서 학생 데이터를 가져오는 API 호출 (시뮬레이션)
+        // 실제 API 호출을 사용하려면 Axios 또는 fetch 등을 사용해야 합니다.
+        // 여기에서는 시뮬레이션을 위해 하드코딩된 데이터를 사용합니다.
+        const simulatedStudentsData = [
+            {
+                name: '김이름',
+                studentNumber: '2019440931',
+                status: 0,
+                authenticationStatus: 2,
+                attendanceTime: '10:00',
+            },
+            {
+                name: '김이름',
+                studentNumber: '2019440932',
+                status: 0,
+                authenticationStatus: 3,
+                attendanceTime: '9:55',
+            },
+            // ... 다른 학생 데이터 추가
+        ];
+
+        this.setState({ studentsData: simulatedStudentsData });
+    }
+
+    setAuthenticationStatus = (status) => {
+        this.setState({ authenticationStatus: status });
+    }
+
+    getStatusText(student) {
+        const statusTexts = ['출석 전', '출석', '지각', '결석'];
+        return statusTexts[student.status];
+    }
+    
+    getAuthenticationStatusText(student) {
+        const authenticationStatusTexts = [
+            '인증되지 않음',
+            '지문인증',
+            '얼굴인증',
+            '지문/얼굴 인증',
+        ];
+        return authenticationStatusTexts[student];
+    }
+
+    getStatusColorClass(student) {
+        const statusColors = ['white-bg', 'blue-bg', 'yellow-bg', 'red-bg'];
+        return statusColors[student.status];
+    }
+    render() {
+        const { authenticationStatus, studentsData } = this.state;
+
+        return (
+            <div className="attendance">
+            {studentsData.map((student) => (
+                <div key={student.studentNumber} className={`profile-state ${this.getStatusColorClass(student)}`}>
+                    <img src={profile} className="profile" alt="profile" />
+                    <p className='name'>{student.name}</p>
+                    <p className='student-id'>{student.studentNumber}</p>
+                    <p className='student-state'>{this.getStatusText(student)}</p>
+                    <p className='student-state'>{this.getAuthenticationStatusText(student.authenticationStatus)}</p>
+                </div>
+            ))}
+        </div>
+        );
+    }
+
+}
+
+export default Attendance;
