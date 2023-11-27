6e1aad863249be46696674cce434750ab23b45db
rtae0 <lk7250@yahoo.com>
2023-11-27 21:21:12 +0900

    Create attendance classification criteria

--- React/src/App.js
+++ React/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.png';
 import logotxt from './logo.svg';
 import Attendance from './Attendance';
-import camera from './camera/camera.jpeg';
+import camera from './profile/camera.jpeg';
 import './App.css';
 
 class App extends Component {
@@ -15,10 +15,32 @@ class App extends Component {
   }
 
   handleStartClass = () => {
-    // 현재 시간을 가져와서 출석 시간 목록에 추가
-    const currentTime = new Date();
+    // 입력된 시간 문자열 가져오기
+    const timeInput = document.querySelector('input[name="time-setting"]');
+    const timeString = timeInput.value;
 
+    // 오늘의 날짜를 가져옴
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = ("0" + (today.getMonth() + 1)).slice(-2); // 월은 0부터 시작하므로 1을 더해줍니다.
+    const day = ("0" + today.getDate()).slice(-2);
+
+    // 시간 문자열을 Date 객체로 파싱
+    const currentTime = new Date(`${year}-${month}-${day}T${timeString}:00`);
+
+    // 유효한 Date 객체인지 확인
+    if (isNaN(currentTime.getTime())) {
+      // 유효하지 않은 경우 얼럿 창 띄우기
+      alert('수업시작 시간을 설정하세요');
+    } else {
+      // 유효한 경우 콘솔에 시간 출력
+      console.log('출석 시간 (Date 객체):', currentTime);
+      this.setState({ classStartTime: currentTime }); // 수업 시작 시간 설정
+
+    }
   };
+
+
   render() {
     const { attendanceTimes } = this.state;
 
@@ -49,15 +71,14 @@ class App extends Component {
               </div>
             </div>
             <div>
-              <button onClick={this.handleStartClass}>출석 시작</button>
+              <button onClick={this.handleStartClass}>출석 확인</button>
             </div>
           </div>
 
           <div className="setting-bar">
             <h2>출석현황</h2>
           </div>
-          <Attendance />
-          
+          <Attendance classStartTime={this.state.classStartTime} />
         </header>
       </div>
     );
--- React/src/Attendance.js
+++ React/src/Attendance.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import profile from './profile_1.png';
+import profile from './profile/profile.png';
 import './App.css';
 
 class Attendance extends Component {
@@ -10,30 +10,65 @@ class Attendance extends Component {
             authenticationStatus: 2, // 초기 인식 상태 (0: 인증하지 않음, 1: 지문인증, 2: 얼굴인증, 3: 지문/얼굴 인증)
         };
     }
+    getStudentImage(student) {
+        const studentImage = `./camera/${student.stu_id}.png`;
+        const defaultImage = profile; // 기본 이미지
 
-    componentDidMount() {
+        try {
+            return require(studentImage); // 학생 이미지를 시도하여 불러옴
+        } catch (error) {
+            return defaultImage; // 오류 발생 시 기본 이미지 반환
+        }
+    }
+    fetchStudentsData() {
+        const { classStartTime } = this.props;
         // 전체 학생 정보 조회
         fetch('http://localhost:3008/users/combined_info', {
             method: 'GET', // GET 요청 방식을 사용
         })
-        .then(response => response.json()) // 서버 응답을 JSON 형식으로 변환
-        .then(data => console.log(data)) // 변환된 데이터를 콘솔에 출력
-        .catch(error => console.error('Error:', error)); // 오류 발생 시 콘솔에 오류 메시지 출력
-    }
-    
-    // componentDidMount() {
-    //     // 전체 학생 정보 조회
-    //     fetch('http://localhost:3008/users/sel_stu', {
-    //         method: 'POST', // POST 요청 방식을 사용
-    //         headers: {
-    //             'Content-Type': 'application/json', // 요청 본문이 JSON 형태임을 나타냄
-    //         }
-    //     })
-    //     .then(response => response.json()) // 서버 응답을 JSON 형식으로 변환
-    //     .then(data => console.log(data)) // 변환된 데이터를 콘솔에 출력
-    //     .catch(error => console.error('Error:', error)); // 오류 발생 시 콘솔에 오류 메시지 출력
-    // }
+            .then(response => response.json()) // 서버 응답을 JSON 형식으로 변환
+            .then(data => {
+                // API에서 받아온 데이터를 studentsData 배열에 추가
+                const updatedStudentsData = data.map(student => {
+                    // 수업 시작 시간이 null이라면 출석 상태를 0으로 설정
+                    if (classStartTime === "") {
+                        student.att_state = 0;
+                    } else {
+                        // 학생의 att_time 정보 가져오기
+                        const studentAttTime = new Date(student.att_time);
+                        // 학생의 출석 상태 계산
+                        if (studentAttTime <= classStartTime) {
+                            student.att_state = 1; // 출석
+                        } else {
+                            const minutesDifference = (studentAttTime - classStartTime) / (1000 * 60); // 분 단위로 차이 계산
 
+                            if (minutesDifference <= 90) { //수업시작 90이내에 오면 지각처리
+                                student.att_state = 2; // 지각
+                            } else {
+                                student.att_state = 3; // 결석
+                            }
+                        }
+                    }
+                    console.log(data)
+                    return student;
+                });
+
+                // 업데이트된 출석 상태를 state에 설정
+                this.setState({ studentsData: updatedStudentsData });
+            })
+            .catch(error => console.error('Error:', error)); // 오류 발생 시 콘솔에 오류 메시지 출력
+    }
+
+
+    componentDidMount() {
+        this.fetchStudentsData();
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.classStartTime !== prevProps.classStartTime) {
+            this.fetchStudentsData();
+        }
+    }
 
     setAuthenticationStatus = (status) => {
         this.setState({ authenticationStatus: status });
@@ -41,8 +76,11 @@ class Attendance extends Component {
 
     getStatusText(student) {
         const statusTexts = ['출석 전', '출석', '지각', '결석'];
-        return statusTexts[student.status];
+        const statusText = statusTexts[student.att_state];
+        console.log(`Student status for ${student.stu_name}: ${statusText}`);
+        return statusText;
     }
+
 
     getAuthenticationStatusText(student) {
         const authenticationStatusTexts = [
@@ -56,26 +94,32 @@ class Attendance extends Component {
 
     getStatusColorClass(student) {
         const statusColors = ['white-bg', 'blue-bg', 'yellow-bg', 'red-bg'];
-        return statusColors[student.status];
+        const status = student.att_state; // 'att_state' 속성을 사용하도록 수정
+
+        if (status >= 0 && status < statusColors.length) {
+            return statusColors[status];
+        } else {
+            return 'default-bg'; // 기본 배경색 클래스
+        }
     }
+
     render() {
         const { authenticationStatus, studentsData } = this.state;
 
         return (
             <div className="attendance">
                 {studentsData.map((student) => (
-                    <div key={student.studentNumber} className={`profile-state ${this.getStatusColorClass(student)}`}>
+                    <div key={student.stu_id} className={`profile-state ${this.getStatusColorClass(student)}`}>
                         <img src={profile} className="profile" alt="profile" />
-                        <p className='name'>{student.name}</p>
-                        <p className='student-id'>{student.studentNumber}</p>
+                        <p className='name'>{student.stu_name}</p>
+                        <p className='student-id'>{student.stu_id}</p>
                         <p className='student-state'>{this.getStatusText(student)}</p>
-                        <p className='student-state'>{this.getAuthenticationStatusText(student.authenticationStatus)}</p>
+                        <p className='student-state'>{this.getAuthenticationStatusText(student.att_way)}</p>
                     </div>
                 ))}
             </div>
         );
     }
-
 }
 
 export default Attendance;
